ba9738d36cbc14ecbef87fae50ab0b0a54ed041c
Bryce Willey <bryce.willey@suffolk.edu>
2025-11-24 16:58:49 -0500

    Use implicit namespaces over explicit namespaces

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 import os
 import sys
-from setuptools import setup, find_packages
+from setuptools import setup, find_namespace_packages
 from fnmatch import fnmatchcase
 from distutils.util import convert_path
 
@@ -51,8 +51,7 @@ setup(name='docassemble.latedocketingstatement',
       author_email='racosta2@su.suffolk.edu',
       license='',
       url='https://docassemble.org',
-      packages=find_packages(),
-      namespace_packages=['docassemble'],
+      packages=find_namespace_packages(),
       install_requires=[],
       zip_safe=False,
       package_data=find_package_data(where='docassemble/latedocketingstatement/', package='docassemble.latedocketingstatement'),
